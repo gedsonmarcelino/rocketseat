9eef5b17cd4f2034639927bfc86452c38eaa86e4
Gedson Marcelino <gedson.marcelino@gmail.com>
2021-12-06 10:26:32 -0300

    Desafio - Refactoring de classes e typescript.

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Route, Switch } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+
+
+const Routes = (): JSX.Element => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+  </Switch>
+);
+
+export default Routes;
